b021c60f0f00fd09bdac5db289f73956554adf7a
Bertoncini <salvatore.bertoncini@accenture.com>
2018-05-27 12:37:09 +0200

    optimization

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ def open_file(path):
 
 	return file_to_read
 
-def print_dataset(dataset):
-	print json.dumps(dataset, indent=2)
+def print_dataset(dataset_name, Npop, dataset):
+	print "dataset: "+dataset_name+", population: "+str(Npop)
+	#print json.dumps(dataset, indent=2)
 
 def fetch_trivial_dataset(trivial_dataset, tag_id, tag_value):	
 	array_replace = trivial_dataset.split()
@@ -33,39 +34,47 @@ def fetch_trivial_dataset(trivial_dataset, tag_id, tag_value):
 	return array_json
 
 
-def fetch_raw_dataset(array_json):
+def fetch_raw_dataset(array_json, tag_id, tag_value):
 	occurrence_json = []
 	population_counter = 0
 
 	for x in array_json:
 		found = 0
 		for y in occurrence_json:
-			if x["user"] == y["user"]:
+			if x[""+tag_id] == y[""+tag_id]:
 				found = 1
 				y["population_counter"] +=1
 		if found == 0:
-			occurrence_json.append({"user": x["user"], "population_counter": 1})
+			occurrence_json.append({""+tag_id: x[""+tag_id], "population_counter": 1})
 		
 		population_counter += 1
 
-	occurrence_json.sort(key=operator.itemgetter('user'))
+	occurrence_json.sort(key=operator.itemgetter(''+tag_id))
 
 	return occurrence_json, population_counter
+
+def build_dataset(dataset_name, tag_id, tag_value):
+	trivial_dataset = open_file('../datasets/'+dataset_name+'.txt')
+	raw_dataset = fetch_trivial_dataset(trivial_dataset, tag_id, tag_value)
+	dataset, Npop = fetch_raw_dataset(raw_dataset, tag_id, tag_value)
+
+	return dataset, Npop
 
 
 if __name__ == '__main__':
 	t0 = time.time()
-	dataset_name = 'domino'
-	trivial_dataset = open_file('../datasets/'+dataset_name+'.txt')
-	raw_dataset = fetch_trivial_dataset(trivial_dataset, 'user', 'resource')
-	json_dataset, Npop = fetch_raw_dataset(raw_dataset)
+
+	resources_dataset_name = 'fire1'
+	resources_dataset, resources_Npop = build_dataset(resources_dataset_name, 'user',  'role')
+
+	users_dataset_name = 'fire2'
+	users_dataset, users_Npop = build_dataset(users_dataset_name, 'role', 'resource')
+
 	t1 = time.time()
 
-	print_dataset(raw_dataset)
-	#print_dataset(json_dataset)
+	print_dataset(resources_dataset_name, resources_Npop, resources_dataset)
+	print_dataset(users_dataset_name, users_Npop, users_dataset)
 
-	print "dataset: "+dataset_name
-	print "population: "+str(Npop)
 	print "time elapsed: "+str(round(t1-t0, 2))+" seconds"
 
 	#access_control_scheme_design(Npop, Tmax, Criteria, Pcros, Pmut)
